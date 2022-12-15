59cd751d4888dab9cf493bb51f31dfc249c9398e
AniruddhaSoni <anirudh.soni007@gmail.com>
2022-12-15 23:15:30 +0530

    updated register js

--- src/register.js
+++ src/register.js
@@ -1,14 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getDocs, Firestore, getFirestore, onSnapshot, query, where, getDoc } from "firebase/firestore";
+import {
+  getDocs,
+  Firestore,
+  getFirestore,
+  onSnapshot,
+  query,
+  where,
+  getDoc,
+} from "firebase/firestore";
 import { collection, addDoc, getDocs } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-async function getdata() { return }
+async function getdata() {
+  return;
+}
 const firebaseConfig = {
   apiKey: "AIzaSyBkx7GcgUZuwcvQBqeCyMHJH10wqIFVkSg",
   authDomain: "tedx-71668.firebaseapp.com",
@@ -16,7 +26,7 @@ const firebaseConfig = {
   storageBucket: "tedx-71668.appspot.com",
   messagingSenderId: "573916814284",
   appId: "1:573916814284:web:04a239cd70f3b41d7edb69",
-  measurementId: "G-LR4J6WY88T"
+  measurementId: "G-LR4J6WY88T",
 };
 
 // Initialize Firebase
@@ -24,17 +34,15 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const analytics = getAnalytics(app);
 
-
 // if there is already user with same email
 async function isvaliddata(q2) {
-
   const querySnapshot = await getDocs(q2);
   console.log(querySnapshot.docs.length);
-  return (querySnapshot.docs.length);
+  return querySnapshot.docs.length;
 }
 // function to adduser details to firebase
 async function adduser(fname, lname, college, branch, contact, email) {
-  const docRef = await addDoc(collection(db, 'users'), {
+  const docRef = await addDoc(collection(db, "users"), {
     firstname: fname,
     lasttname: lname,
     college: college,
@@ -45,57 +53,54 @@ async function adduser(fname, lname, college, branch, contact, email) {
   console.log("Document written with ID: ", docRef.id);
 }
 
-
 function logSubmit(event) {
-
-
+  event.preventDefault();
 
   const formData = new FormData(event.target);
   const formProps = Object.fromEntries(formData);
   // if any of the field is empty
-  if (formProps.fname == "" || formProps.lname == "" || formProps.college == "" || formProps.branch == "" || formProps.contact == "") {
-    alert("enter all valid details")
-  }
-  
-  else {
+  if (
+    formProps.fname == "" ||
+    formProps.lname == "" ||
+    formProps.college == "" ||
+    formProps.branch == "" ||
+    formProps.contact == ""
+  ) {
+    alert("enter all valid details");
+  } else {
     try {
       var contact = formProps.email;
-      const docref = collection(db, 'users');
+      const docref = collection(db, "users");
       const q2 = query(collection(db, "users"), where("email", "==", contact));
       // function will return promise
       let l = isvaliddata(q2);
       l.then(
         function (value) {
           if (value == 0) {
-            adduser(formProps.fname,
-              formProps.lname, formProps.college, formProps.branch, formProps.contact, formProps.email);
-            alert("your response has been accepted ")
-              document.getElementById("register").reset();
-            }
-
-
-          else {
+            adduser(
+              formProps.fname,
+              formProps.lname,
+              formProps.college,
+              formProps.branch,
+              formProps.contact,
+              formProps.email
+            );
+            alert("your response has been accepted ");
+            document.getElementById("register").reset();
+          } else {
             alert("user is already registered use different mail");
           }
-
         },
-        function (error) { console.log(error) }
+        function (error) {
+          console.log(error);
+        }
       );
-
-
-
-
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   }
-  event.preventDefault();
-
-
-
 }
 
-const form = document.getElementById('register');
+const form = document.getElementById("register");
 
-
-form.addEventListener('submit', logSubmit);
+form.addEventListener("submit", logSubmit);
